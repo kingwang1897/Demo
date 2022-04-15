59d7721a0d2381569e63d2637a970bff625cd05a
kingwang1897 <king.wang@storicard.com>
2022-04-15 19:23:01 +0800

    init

--- app/web/src/main/java/com/alipay/sofa/Test.java
+++ app/web/src/main/java/com/alipay/sofa/Test.java
@@ -1,45 +1,76 @@
 package com.alipay.sofa;
 
+import com.alipay.sofa.constant.Constant;
 import com.alipay.sofa.rpc.common.json.JSON;
+import com.alipay.sofa.util.CommonUtil;
 import com.solab.iso8583.IsoMessage;
 import com.solab.iso8583.IsoType;
 import com.solab.iso8583.MessageFactory;
-import com.solab.iso8583.impl.SimpleTraceGenerator;
 import com.solab.iso8583.parse.ConfigParser;
 
 public class Test {
-    private static int hexToInt(int n) {
-        return Integer.parseInt(String.valueOf(n), 16);
+    public static void main(String[] args) throws Exception {
+        // genarate iso8583 message
+        String message = genarateMessage();
+        System.out.println("genarateMessage result is:" + message);
+
+        // parse iso8583 message
+        parseMessage(message);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static String genarateMessage() throws Exception {
         MessageFactory<IsoMessage> mf = new MessageFactory<IsoMessage>();
         mf.setBinaryHeader(true);
         mf.setBinaryFields(false);
         mf.setUseBinaryBitmap(false);
         mf.setForceStringEncoding(true);
         mf.setUseBinaryMessages(false);
-
         mf.setAssignDate(true);
-        mf.setTraceNumberGenerator(new SimpleTraceGenerator((int) (System.currentTimeMillis() % 100000)));
         System.out.println("NEW MESSAGE");
 
-        IsoMessage m = mf.newMessage(hexToInt(800));
-        m.setIsoHeader("46013036303434383031303030303030303030303130303030303030000000003030303030303030003030303030");
+        IsoMessage m = mf.newMessage(Integer.parseInt("800", 16));
         m.setValue(7, "0413140000", IsoType.DATE10, 10);
         m.setValue(11, "480752", IsoType.NUMERIC, 6);
-        m.setValue(48, "4e4b313233343536373839306162636465663132333435363738393061626364656631323334353637383930616263646566313233343536373839306162636465663132333435363738393061626364656631323334353637383930616263646566313233343536373839306162636465663132333435363738393061626364656631323334353637383930616263646566313233343536373839306162636465663132333435363738393061626364656631323334353637383930616263646566313233343536373839306162636465663132333435363738393061626364656631323334353637383930616263646566313233343536373839306162636130383030822000000001080004000001100000003034313231343031363030383030822000000001080004000001100000003034313231343030303030383030822000000001080004000001100000003034313231343030303030383030822000000001080004000001100000003034313231343030303030383030822000000001080004000001100000003034313231343030303030383030822000000001080004000001100000003034313231343030303030383030822000000001080004000001100000003034313231343030303030383030822000000001080004000001100000003034313231343030303030383030323134303231343032313430", IsoType.LLLLBIN, 1024);
+        m.setValue(48, "NK23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D26125686364C23D36C44439519F136EE2069998D21", IsoType.LLLBIN, 512);
         m.setValue(53, "1600000000000000", IsoType.NUMERIC, 16);
         m.setValue(70, "101", IsoType.NUMERIC, 3);
-        m.setValue(96, "0000000000000000", IsoType.ALPHA, 16);
+        m.setValue(96, "00000000", IsoType.ALPHA, 8);
         m.setValue(100, "92010000", IsoType.LLVAR, 11);
-        m.setValue(128, "4346414646314336", IsoType.ALPHA, 16);
+        m.setValue(128, "ABCDEFGH", IsoType.ALPHA, 8);
+
+        String messageHeader = messageHeader(m.debugString().length() + 46);
+        m.setIsoHeader(messageHeader);
 
         m.setForceSecondaryBitmap(true);
         System.out.println(m.debugString());
-        print(m);
 
+        // 报文头
+        System.out.println("----IsoHeader---");
+        Integer headerIndex = m.getIsoHeader().length();
+        System.out.println(messageHeader);
+        // 报文业务类型
+        System.out.println("----IsoTypeID---");
+        String messageTypeID = CommonUtil.convertStringToHex(m.debugString().substring(headerIndex, headerIndex + 4));
+        System.out.println(messageTypeID);
+        // 报文内容
+        System.out.println("----IsoBitMap---");
+        Integer bitMapLength = m.isBinaryHeader() || m.isBinaryHeader() ? 16 : 32;
+        String messageBitMap = m.debugString().substring(headerIndex + 4, headerIndex + 4 + bitMapLength);
+        System.out.println(messageBitMap);
+        //
+        System.out.println("----IsoData---");
+        Integer messageIndex = headerIndex + 4 + bitMapLength;
+        String messageDataResouce = m.debugString().substring(messageIndex);
+        String messageData = CommonUtil.convertStringToHex(messageDataResouce);
+        System.out.println(messageData);
 
+        System.out.println("----ALL(HEX)---");
+        String message = messageHeader + messageTypeID + messageBitMap + messageData;
+        System.out.println(message);
+        return message;
+    }
+
+    public static void parseMessage(String message) throws Exception{
         //初始化获取解析配置文件j8583.xml
         MessageFactory messageFactory = ConfigParser.createDefault();
         //与组包一致
@@ -49,27 +80,17 @@ public class Test {
         messageFactory.setForceStringEncoding(true);
         messageFactory.setUseBinaryMessages(false);
 
-        //解析报文
-        IsoMessage isoMessage = messageFactory.parseMessage(m.debugString().getBytes(), 0, true);
-        print(isoMessage);
-    }
-
-    // 输出一个报文内容
-    private static void printIn(IsoMessage m) {
-        System.out.println("----------------------------------------------------- ");
+        String messageData = CommonUtil.convertHexToString(message.substring(92 + 8 + 32));
         StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append(m.getIsoHeader());
-        stringBuffer.append(Integer.toHexString(m.getType()));
-        System.out.println("Message Header = [" + m.getIsoHeader() + "]");
-        System.out.println("Message TypeID = [" + m.getType() + "]");
-        for (int i = 2; i <= 128; i++) {
-            if (m.hasField(i)) {
-                System.out.println("FieldID: " + i
-                        + " <" + m.getField(i).getType()
-                        + ">\t[" + JSON.toJSONString(m.getObjectValue(i))
-                        + "]\t[" + m.getField(i).toString() + "]");
-            }
-        }
+        stringBuffer.append(message.substring(0, 92))
+                .append(CommonUtil.convertHexToString(message.substring(92, 92 + 8)))
+                .append(message.substring(92 + 8, 92 + 8 +  32))
+                .append(messageData);
+        System.out.println(stringBuffer.toString());
+
+        //解析报文
+        IsoMessage isoMessage = messageFactory.parseMessage(stringBuffer.toString().getBytes(), 92, true);
+        print(isoMessage);
     }
 
     // 输出一个报文内容
@@ -87,17 +108,19 @@ public class Test {
         }
     }
 
-    public static String stringToAscii(String value) {
-        StringBuffer sbu = new StringBuffer();
-        char[] chars = value.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if(i != chars.length - 1) {
-                sbu.append((int)chars[i]).append(",");
-            } else {
-                sbu.append((int)chars[i]);
-            }
-        }
-        return sbu.toString();
-    }
+    public static String messageHeader(Integer messagelength){
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(Integer.toHexString(Constant.MESSAGE_HEALDER_LENGTH))
+                        .append(CommonUtil.convertInergerToHex(Constant.MESSAGE_HEALDER_FLAG))
+                        .append(CommonUtil.convertStringToHex(CommonUtil.addContentBylength(messagelength.toString(), Constant.MESSAGE_HEALDER_MESSAGE_LENGTH, Constant.MESSAGE_HEALDER_MESSAGE_LENGTH_CONTENT)))
+                        .append(CommonUtil.convertStringToHex(Constant.MESSAGE_HEALDER_DESTINATION))
+                        .append(CommonUtil.convertStringToHex(Constant.MESSAGE_HEALDER_SOURCE))
+                        .append(CommonUtil.convertInergerToHex(Constant.MESSAGE_HEALDER_RESERVED))
+                        .append(CommonUtil.convertInergerToHex(Constant.MESSAGE_HEALDER_BATCH))
+                        .append(CommonUtil.convertStringToHex(Constant.MESSAGE_HEALDER_TRANSACTION))
+                        .append(CommonUtil.convertInergerToHex(Constant.MESSAGE_HEALDER_USER))
+                        .append(CommonUtil.convertStringToHex(Constant.MESSAGE_HEALDER_REJECT));
 
+        return stringBuffer.toString();
+    }
 }
---
+++ app/web/src/main/java/com/alipay/sofa/constant/Constant.java
@@ -0,0 +1,15 @@
+package com.alipay.sofa.constant;
+
+public class Constant {
+    public static Integer MESSAGE_HEALDER_LENGTH = 46;
+    public static String MESSAGE_HEALDER_FLAG = "12";
+    public static Integer MESSAGE_HEALDER_MESSAGE_LENGTH = 4;
+    public static String MESSAGE_HEALDER_MESSAGE_LENGTH_CONTENT = "0";
+    public static String MESSAGE_HEALDER_DESTINATION = "48010000000";
+    public static String MESSAGE_HEALDER_SOURCE = "00010000000";
+    public static String MESSAGE_HEALDER_RESERVED = "000000";
+    public static String MESSAGE_HEALDER_BATCH = "00";
+    public static String MESSAGE_HEALDER_TRANSACTION = "00000000";
+    public static String MESSAGE_HEALDER_USER = "00";
+    public static String MESSAGE_HEALDER_REJECT = "00000";
+}
